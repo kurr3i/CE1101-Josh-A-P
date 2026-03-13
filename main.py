aea87da7c17ca2ce338aefb4ee19d903b4887ca0
kurr3i <joshuandrade777@gmail.com>
2026-03-12 18:55:27 -0600

    Primer avance tarea #1, creación de la función tec_info.get_about_info() y su importación a el código principal main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+
+
+
+
+#Creación de una librería propia
+import tec_info
+r=tec_info.get_about_info()
+print(r)
---
+++ tec_info.py
@@ -0,0 +1,8 @@
+import sys
+def get_about_info():
+    return """Universidad: Instituto Tecnológico de Costa Rica
+    Escuela de Ingeniería en Computadores
+    Curso: Introducción a la Programación (CE1101)
+    Año: 2026
+    Grupo 4
+    Versión de Python: """ + str(sys.version)
